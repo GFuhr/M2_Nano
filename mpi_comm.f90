af8de06c0e9b32e1bac67fa1b91042c673d4ee32
G. Fuhr <guiyom00@gmail.com>
2019-02-01 23:59:35 +0100

    typo et instructions

--- mpi_comm.f90
+++ mpi_comm.f90
@@ -1,7 +1,7 @@
 module mpicomm
     use mpi
     use geometry
-    use field, only u_t
+    use fields, only:u_t
 
     implicit none
 
@@ -12,7 +12,8 @@ module mpicomm
     integer ::ierr
     integer, parameter :: master = 0
     integer, parameter:: ndims = 2
-
+    integer ::nxlocal, nylocal
+    real(kind=8) ::starttime, endtime
     integer, dimension(2) :: voisins
 
 contains 
@@ -33,20 +34,21 @@ subroutine init_mpi
     
 
     ! start timer
-  if (myid .EQ. 0) then
+  if (myid .EQ. master) then
   starttime =  MPI_WTIME()
   else
     starttime = -1
   end if
 end subroutine init_mpi
 
-subroutine MPI_boundary(field)
-    real(kind=8), intent(in out), dimension(:,:)::field
-
-end subroutine MPI_boundary
+! if you need to send data between processors during RK scheme computation, you can uncomment and call this subroutine
+!subroutine MPI_boundary(field)
+!    real(kind=8), intent(in out), dimension(:,:)::field
+!
+!end subroutine MPI_boundary
 
 subroutine mpi_end
-if (myid .EQ. 0)
+if (myid .EQ. master) then
 endtime=  MPI_WTIME()
 print *, "total time", endtime - starttime
 end if
